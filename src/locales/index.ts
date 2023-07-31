7caf06f9ce7b691da4cbd880116a95cb667fcc34
thanasislanaras <57492658+Thanasis09@users.noreply.github.com>
2023-08-01 02:05:59 +0300

    Fixed Translations

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,7 +1,23 @@
 import el from './el.json'
 import en from './en.json'
+import fr from './fr.json'
+import ag from './ag.json'
+import es from './es.json'
+import tl from './tl.json'
+import it from './tl.json'
+import de from './de.json'
+import pirate from './pirate.json'
+import enderman from './enderman.json'
 
 export default {
   en,
-  el
+  el,
+  fr,
+  ag,
+  es,
+  tl,
+  it,
+  de,
+  pirate,
+  enderman
 }
--- src/shared/core/locale.ts
+++ src/shared/core/locale.ts
@@ -3,7 +3,23 @@ export default function getLanguageName(locale: string): string {
     case 'en':
       return 'English'
     case 'el':
-      return 'Greek'
+      return 'Ελληνικά'
+    case 'fr':
+      return 'Français'
+    case 'ag':
+      return 'Ἀρχαῖα Ἑλληνική'
+    case 'es':
+      return 'Espanol'
+    case 'tl':
+      return 'Tagalog'
+    case 'it':
+      return 'Italiano'
+    case 'de':
+      return 'Deutsch'
+    case 'pirate':
+      return 'Pirate English'
+    case 'enderman':
+      return 'egagnahdE egaugnaL'
     default:
       return locale
   }
