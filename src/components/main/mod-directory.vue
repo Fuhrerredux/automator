299d4d5050a4e190ed099ebb7110ac699be342f9
dax <isaiahcollins02@gmail.com>
2023-08-01 15:43:39 +0800

    Fixed traits not loading

--- src/components/main/mod-directory.vue
+++ src/components/main/mod-directory.vue
@@ -2,9 +2,11 @@
 import { useI18n } from 'vue-i18n'
 import { ExclamationTriangleIcon, FolderIcon } from '@heroicons/vue/24/outline'
 import useModStore from '@stores/mod'
+import useTraitStore from '@stores/traits'
 import { open } from '@tauri-apps/api/dialog'
 
-const store = useModStore()
+const modStore = useModStore()
+const traitsStore = useTraitStore()
 const { t } = useI18n()
 
 async function browse() {
@@ -12,15 +14,16 @@ async function browse() {
     directory: true
   })
   if (directory && typeof directory === 'string') {
-    store.$patch({ directory })
+    modStore.$patch({ directory })
     localStorage.setItem('directory', directory)
+    traitsStore.readDir(directory)
   }
 }
 </script>
 
 <template>
   <div
-    v-if="store.directory.length <= 0"
+    v-if="modStore.directory.length <= 0"
     class="mx-auto w-1/2 text-sm text-center bg-amber-100 dark:bg-amber-500 dark:text-amber-800 font-medium p-4 rounded-md">
     <exclamation-triangle-icon class="h-6 w-6 inline-block" />
     <p>{{ t('error.no-dir') }}</p>
@@ -34,7 +37,7 @@ async function browse() {
         name="directory"
         id="directory"
         class="form-input"
-        v-model="store.directory" />
+        v-model="modStore.directory" />
       <button type="button" class="button-primary flex items-center shrink-0" @click="browse">
         <folder-icon class="h-5 w-5 mr-2" />
         <span>{{ t('action.browse') }}</span>
