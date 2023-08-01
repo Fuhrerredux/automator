17bb0ab4393ee05a636506088d924c9e9f410c66
dax <isaiahcollins02@gmail.com>
2023-08-01 08:26:09 +0800

    Update Translations

---
+++ src/components/others/character-recruit.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import { useToast } from 'vue-toast-notification'
+import ActionItem from '@components/action-item.vue'
+import { UserIcon } from '@heroicons/vue/24/outline'
+
+const { t } = useI18n()
+const $toast = useToast()
+
+async function append() {
+  $toast.success('sucess!')
+}
+</script>
+
+<template>
+  <action-item
+    :title="t('others.character-recruit.heading')"
+    :summary="t('others.character-recruit.summary')"
+    @action="append">
+    <template #icon>
+      <user-icon class="h-6 w-6" />
+    </template>
+  </action-item>
+</template>
--- src/pages/others.vue
+++ src/pages/others.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
+import CharacterRecruit from '@components/others/character-recruit.vue'
 import ShineGeneration from '@components/others/shine-generation.vue'
 
 const { t } = useI18n()
@@ -11,8 +12,9 @@ const { t } = useI18n()
       <h1 class="header">{{ t('route.others') }}</h1>
     </div>
     <div class="mt-4">
-      <ul>
+      <ul class="space-y-4">
         <shine-generation />
+        <character-recruit />
       </ul>
     </div>
   </main>
