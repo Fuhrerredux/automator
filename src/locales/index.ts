7f0401e98d86312d6e8ce854ee1291e51a0ad0db
thanasislanaras <57492658+Thanasis09@users.noreply.github.com>
2024-01-31 14:01:52 +0200

    feat: fix locales and add french locale

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -2,10 +2,12 @@ import de from './de.json'
 import el from './el.json'
 import en from './en.json'
 import it from './it.json'
+import fr from './fr.json'
 
 export default {
   en,
   el,
   it,
-  de
+  de,
+  fr
 }
