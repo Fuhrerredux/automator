2ee43f297e6e771d2bb14eb0362c048be75cf7d4
thanasislanaras <57492658+Thanasis09@users.noreply.github.com>
2024-04-09 18:27:11 +0300

    fix(rust): rm unused import

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::window;
 use tauri::{Window, Manager};
 use tracing_subscriber::filter::LevelFilter;
 use tracing_subscriber::fmt;
